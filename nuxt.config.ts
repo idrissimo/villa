b2e3f78ec2be569b288435ff5d4393113a97d9f8
idrissi <addonworx@outlook.com>
2024-02-05 20:56:24 +0100

    add header css and html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,45 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  app:{
+    head: {
+      viewport: 'width=device-width, initial-scale=1.0',
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap',
+          crossorigin: ''
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/locomotive-scroll.css',
+          
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css',
+          
+        },
+
+      ],
+      title:'Villa Noure',
+    }
+    
+  }
   
 
 })
