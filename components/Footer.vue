eaf05ae841c037702acf2c65868eff4afb166517
idrissi <addonworx@outlook.com>
2024-02-07 16:44:32 +0100

    footer

---
+++ components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+ <div id="footer-wrapper">
+    <div id="footer">
+        <div id="social-icons">
+            <a href="#"><i class="ri-facebook-circle-fill"></i></a> 
+            <a href="#"><i class="ri-youtube-fill"></i></a> 
+            <a href="#"><i class="ri-instagram-fill"></i></a>
+        </div>
+        <div id="footer-menu">
+            <ul id="menu-footer1" class="menu flex gap-15">
+                <li><a href="https://yoyo-rent.com/qui-sommes-nous/">Qui sommes-nous</a></li>
+                <li><a href="https://yoyo-rent.com/conditions-de-location/">Conditions générales de location</a></li>
+                <li><a href="https://yoyo-rent.com/contactez-nous/">Contactez-nous</a></li>
+            </ul>
+        </div>
+        <div id="footer-logo">
+            <i class="ri-bluesky-fill"></i>
+            <span>VILLA NOURE</span>
+        </div>
+    </div>
+            
+</div>
+
+
+</template>
+
+<style>
+#footer-wrapper {
+    background-color: var(--primary-color);
+    color: white;
+}
+
+
+#footer {
+    padding: 2rem;
+    max-width: 1440px;
+    margin: 0 auto;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 15px;
+    list-style: none;
+}
+
+#footer #social-icons, #footer-logo i {
+    font-size: 1.5rem;
+}
+
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
     <Intro></Intro>
     <Gallery></Gallery>
+    <Footer></Footer>
     <div class="home">
       <h1>Welcome to my website</h1>
       <p>This is the homepage of my website.</p>
